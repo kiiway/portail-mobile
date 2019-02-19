cb0b94ef5021737d8e0c1da5304e7b2c79cb0e36
CdRom1 <r.maliach@live.fr>
2019-02-19 16:50:04 +0100

    Implémentation d'exemples pour la liste

--- src/screens/Notifications/index.js
+++ src/screens/Notifications/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { View, Image, Text, ScrollView, Button } from 'react-native';
-import styles from '../../styles'
-import { colors } from '../../styles/variables';
-import Storage from '../../services/Storage';
+import { View, Text} from 'react-native';
+import List from '../../components/List';
 
+const forMembersData = [
+	{ icon: {uri: 'https://www.utc.fr/fileadmin/user_upload/SITE-UTC/images/Home/Logotheque/sigle_UTC.jpg'},			text: "Suivre et être notifié des actualités de l'UTC et de vos associations préférées" },
+	{ icon: "map",			text: "Se repérer dans le campus" },
+	{ icon: "bell",			text: "Etre notifié des résultats des examens et d'événements qui vous intéressent" },
+	{ icon: "calendar",		text: "Consulter et synchroniser son agenda scolaire, associatif et personnel" },
+]
 
 export default class NotificationsScreen extends React.Component {
 	render() {
 		return (
-			<Text>Notifications Screen!</Text>
+			<View>
+				<Text>Notifications Screen!</Text>
+				<List data={forMembersData}/>
+			</View>
 		);
 	}
 }
